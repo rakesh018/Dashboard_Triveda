f684e2b116b09defc909dc45d728e9517693ba58
Sai Kiran <neelakantamsaikiran7@gmail.com>
2024-09-04 15:07:19 +0530

    mailto

--- src/pages/ClientsDashboard.jsx
+++ src/pages/ClientsDashboard.jsx
@@ -13,7 +13,7 @@ export default function ClientsDashboard() {
         <h1 className='text-3xl font-semibold text-gray-50'>Clients Dashboard</h1>
       </div>
       
-      <div className='bg-gray-900 shadow-md rounded-md p-6'>
+      <div className='bg-gray-900 shadow-md rounded-md p-6 font-opensans'>
         <table className='w-full text-sm text-left text-gray-700'>
           <thead className='text-xs uppercase bg-gray-950 text-gray-50'>
             <tr>
@@ -26,7 +26,7 @@ export default function ClientsDashboard() {
             {clients.map((client, index) => (
               <tr key={index} className={`bg-gray-950 ${index !== clients.length-1 ? 'border-b':''}  border-gray-50`}>
                 <th scope='row' className='px-6 py-4 font-medium text-gray-50'>{client.name}</th>
-                <td className='px-6 py-4 text-gray-50'>{client.email}</td>
+                <td className='px-6 py-4 text-gray-50'><a href={`mailto:${client.email}`}>{client.email}</a></td>
                 <td className='px-6 py-4 text-gray-50'>{client.purpose}</td>
               </tr>
             ))}
