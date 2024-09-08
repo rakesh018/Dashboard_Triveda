df4ed115355450b02863f52d9e5bd23b1e732d0b
Sai Kiran <neelakantamsaikiran7@gmail.com>
2024-09-08 09:21:11 +0530

    impovements

--- src/pages/ClientsDashboard.jsx
+++ src/pages/ClientsDashboard.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 export default function ClientsDashboard() {
   const clients = [
-    { name: 'Luffy', email: 'luffy@gmail.com', purpose: 'App Development' },
-    { name: 'Zoro', email: 'zoro@gmail.com', purpose: 'App Development' },
-    { name: 'Sanji', email: 'sanji@gmail.com', purpose: 'Web Development' },
+    { name: 'Luffy', email: 'luffy@gmail.com', purpose: 'App Development', phone: '123-456-7890' },
+    { name: 'Zoro', email: 'zoro@gmail.com', purpose: 'App Development', phone: '987-654-3210' },
+    { name: 'Sanji', email: 'sanji@gmail.com', purpose: 'Web Development', phone: '555-555-5555' },
   ];
+  
 
   return (
     <div className='w-full ml-64 min-h-screen bg-black p-8 text-textColor-light font-opensans'>
@@ -18,14 +19,17 @@ export default function ClientsDashboard() {
           <thead className='text-xs uppercase bg-gray-950 text-gray-50'>
             <tr>
               <th scope='col' className='px-6 py-3'>Client name</th>
+              <th scope='col' className='px-6 py-3'>Phone Number</th>
               <th scope='col' className='px-6 py-3'>Email</th>
-              <th scope='col' className='px-6 py-3'>Purpose</th>
+              <th scope='col' className='px-6 py-3'>Work</th>
             </tr>
           </thead>
           <tbody>
             {clients.map((client, index) => (
               <tr key={index} className={`bg-gray-950 ${index !== clients.length-1 ? 'border-b':''}  border-gray-50`}>
                 <th scope='row' className='px-6 py-4 font-medium text-gray-50'>{client.name}</th>
+                <th scope='row' className='px-6 py-4 font-medium text-gray-50'>{client.phone}</th>
+
                 <td className='px-6 py-4 text-gray-50'><a href={`mailto:${client.email}`}>{client.email}</a></td>
                 <td className='px-6 py-4 text-gray-50'>{client.purpose}</td>
               </tr>
--- src/pages/WebsiteDashboard.jsx
+++ src/pages/WebsiteDashboard.jsx
@@ -7,6 +7,7 @@ export default function WebsiteDashboard() {
                 <h1 className='text-3xl font-semibold text-gray-50'>Website Dashboard</h1>
             </div>
 
+          
             <div className='bg-gray-900 shadow-md rounded-md p-6 mb-8'>
                 <h1 className='text-xl font-semibold text-gray-50'>Services</h1>
                 <div className='mt-4 flex flex-row gap-4 items-center'>
@@ -15,6 +16,7 @@ export default function WebsiteDashboard() {
                         placeholder='Add New Service'
                         className='bg-gray-800 border border-gray-700 text-gray-200 w-[500px] px-4 py-2 rounded-md outline-none focus:ring-2 focus:ring-gray-600'
                     />
+                  
                     <button
                         type="button"
                         className="text-white font-bold bg-violet-950 hover:bg-violet-600 focus:ring-4 rounded-md text-sm px-5 py-3"
@@ -22,6 +24,7 @@ export default function WebsiteDashboard() {
                         Add Service
                     </button>
                 </div>
+               
             </div>
 
             <div className='bg-gray-900 shadow-md rounded-md p-6 mb-8'>
